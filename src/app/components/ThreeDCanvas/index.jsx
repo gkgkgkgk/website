34f6bd7bbd0692ac9d2831d102859f83bdc31789
Gavri Kepets <gkepets@gmail.com>
2023-09-29 15:18:49 -0400

    tif

--- src/app/components/ThreeDCanvas/index.jsx
+++ src/app/components/ThreeDCanvas/index.jsx
@@ -1,48 +1,150 @@
 import React, { useEffect } from 'react';
 import * as THREE from 'three';
+const { Noise } = require('noisejs');
+import {fromBlob} from 'geotiff'
 
 const ThreeDCanvas = () => {
+  const generateMountainRange = async (mountainHeight) => {
+    const noise = new Noise();
+
+    const width = 512;
+    const height = 512;
+
+    const size = width * height;
+    const data = new Uint8Array( 4 * size );
+
+    for ( let i = 0; i < size; i ++ ) {
+      const x = i % width;
+      const y = Math.floor(i / width);
+      let value = noise.simplex2(x / 100, y / 100);
+      value = (value + 1) / 2;
+      const stride = i * 4;
+      data[ stride ] = value* 255;
+      data[ stride + 1 ] = value * 255;
+      data[ stride + 2 ] = value* 255;
+      data[ stride + 3 ] = 255;
+    }
+
+    const texture = new THREE.DataTexture( data, width, height );
+    texture.needsUpdate = true;
+
+    const colorData = new Uint8Array( 4 * size );
+
+    for ( let i = 0; i < size; i ++ ) {
+      const stride = i * 4;
+      if(data[stride] < 20){
+        colorData[ stride ] = 255;
+        colorData[ stride + 1 ] = 255;
+        colorData[ stride + 2 ] = 255;
+        colorData[ stride + 3 ] = 255;
+      } else if(data[stride] < 100){
+        colorData[ stride ] = 100;
+        colorData[ stride + 1 ] = 100;
+        colorData[ stride + 2 ] = 100;
+        colorData[ stride + 3 ] = 255;
+      } else {
+        colorData[ stride ] = 25;
+        colorData[ stride + 1 ] = 255;
+        colorData[ stride + 2 ] = 50;
+        colorData[ stride + 3 ] = 255;
+      }
+    }
+
+    const color = new THREE.DataTexture( colorData, width, height );
+    color.needsUpdate = true;
+
+    let terrain = 'tifs/agri-medium-dem.tif'
+    const terrainResponse = await fetch(terrain);
+    const terrainBlob = await terrainResponse.blob();
+    const rawTiff = await fromBlob(terrainBlob);
+    const tifImage = await rawTiff.getImage();
+
+    // Get the dimensions of the GeoTIFF image
+    const imageWidth = tifImage.getWidth();
+    const imageHeight = tifImage.getHeight();
+    console.log(imageWidth, imageHeight)
+    // Read elevation data from the GeoTIFF file
+    const rawElevationData = await tifImage.readRasters({
+      interleave: true,
+      width: 125,
+      height: 98,
+    });
+    console.log(rawElevationData)
+    // Normalize elevation data
+    const minValue = Math.min(...rawElevationData);
+    const maxValue = Math.max(...rawElevationData);
+    console.log(minValue, maxValue)
+    const elevationData = rawElevationData.map((value) => (value - minValue) / (maxValue - minValue));
+
+    // Create terrain geometry
+    const geometry = new THREE.PlaneGeometry(20, 20, 125 - 1, 98 - 1);
+
+    // Apply elevation data to the vertices
+    const positions = geometry.attributes.position.array;
+    for (let i = 0; i < positions.length; i += 3) {
+      const x = positions[i];
+      const y = positions[i + 1];
+      const elevation = elevationData[i / 3] * mountainHeight; // Adjust the multiplier for vertical scaling
+      positions[i + 2] = elevation;
+    }
+
+
+    const imagetexture = new THREE.TextureLoader().load('tifs/agri-medium-autumn.jpg');
+    const material = new THREE.MeshStandardMaterial({
+      map: imagetexture,
+    });
+
+    const plane = new THREE.Mesh(geometry, material);
+    plane.rotation.x = -Math.PI/2;
+    plane.position.z = -10
+    plane.receiveShadow = true;
+    plane.castShadow = true;
+
+    return plane;
+  }
+
+
+
   useEffect(() => {
-    // Create a scene
     const scene = new THREE.Scene();
+    // const camera = new THREE.OrthographicCamera(0,0,0,0, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(25, 1, 0.1, 10000); // Aspect ratio set to 1 initially
 
-    // Create a camera
-    const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000); // Aspect ratio set to 1 initially
-    camera.position.z = 5;
-
-    // Check if the container already has children (canvas)
+    camera.position.z = 20;
     const canvas = document.getElementById('canvas');
     const container = document.getElementById('three-container');
-    // Create a renderer
-    const renderer = new THREE.WebGLRenderer({ alpha: true, canvas });
+    const renderer = new THREE.WebGLRenderer({ alpha: true, canvas, antialias:true });
 
     const setRendererSize = () => {
       const width = container.clientWidth;
       const height = container.clientHeight;
       renderer.setSize(width, height);
+
       camera.aspect = width / height;
       camera.updateProjectionMatrix();
     };
 
-    // Call the function initially and when the window is resized
     setRendererSize();
 
-    renderer.setClearColor(0x000000, 0.5); // Color, Alpha
+    renderer.setClearColor(0x000000, 0.0);
+    let mountains = null;
+    const setupScene = async () => {
+      let mountainHeight = 4;
+      mountains = await generateMountainRange(mountainHeight);
+      const directionalLight = new THREE.DirectionalLight(0xffd29c, 2);
+      directionalLight.castShadow = true;
+      directionalLight.position.set(5, 1, 0);
+      scene.add(directionalLight);
+      scene.add(mountains);
+    }
+    
+    setupScene();
 
-    // Create a cube
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    // Animation function
     const animate = () => {
       requestAnimationFrame(animate);
-
-      // Rotate the cube
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
+      if(mountains){
+        mountains.position.y = -6;
+      }
       renderer.render(scene, camera);
     };
 
@@ -54,10 +156,8 @@ const ThreeDCanvas = () => {
       document.documentElement.style.setProperty('--vh', `${vh}px`);
     };
 
-    // Handle window resize
     window.addEventListener('resize', handleResize);
 
-    // Clean up on unmount
     return () => {
       window.removeEventListener('resize', handleResize);
     };
