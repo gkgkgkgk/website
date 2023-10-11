de4073d4d7df1435032f28dae5e2ab5a3d818032
Gavri Kepets <gkepets@gmail.com>
2023-10-11 00:14:36 -0400

    init clouds

--- src/app/components/ThreeDCanvas/index.jsx
+++ src/app/components/ThreeDCanvas/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import * as THREE from 'three';
 const { Noise } = require('noisejs');
 import {fromBlob} from 'geotiff'
+import { ImprovedNoise } from 'three/addons/math/ImprovedNoise.js';
 
 const ThreeDCanvas = () => {
   const generateMountainRange = async (mountainHeight) => {
@@ -109,7 +110,8 @@ const ThreeDCanvas = () => {
     const scene = new THREE.Scene();
     // const camera = new THREE.OrthographicCamera(0,0,0,0, 0.1, 1000);
     const camera = new THREE.PerspectiveCamera(25, 1, 0.1, 10000); // Aspect ratio set to 1 initially
-
+    let material;
+    let mesh;
     camera.position.z = 20;
     const canvas = document.getElementById('canvas');
     const container = document.getElementById('three-container');
@@ -136,6 +138,98 @@ const ThreeDCanvas = () => {
       directionalLight.position.set(5, 1, 0);
       scene.add(directionalLight);
       scene.add(mountains);
+
+      const size = 128;
+				const data = new Uint8Array( size * size * size );
+
+				let i = 0;
+				const scale = 0.05;
+				const perlin = new ImprovedNoise();
+				const vector = new THREE.Vector3();
+
+				for ( let z = 0; z < size; z ++ ) {
+
+					for ( let y = 0; y < size; y ++ ) {
+
+						for ( let x = 0; x < size; x ++ ) {
+
+							const d = 1.0 - vector.set( x, y, z ).subScalar( size / 2 ).divideScalar( size ).length();
+							data[ i ] = ( 128 + 128 * perlin.noise( x * scale / 1.5, y * scale, z * scale / 1.5 ) ) * d * d;
+							i ++;
+
+						}
+
+					}
+
+				}
+
+				const texture = new THREE.Data3DTexture( data, size, size, size );
+				texture.format = THREE.RedFormat;
+				texture.minFilter = THREE.LinearFilter;
+				texture.magFilter = THREE.LinearFilter;
+				texture.unpackAlignment = 1;
+				texture.needsUpdate = true;
+
+				// Material
+
+				const vertexShader = /* glsl */`
+					in vec3 position;
+
+					uniform mat4 modelMatrix;
+					uniform mat4 modelViewMatrix;
+					uniform mat4 projectionMatrix;
+					uniform vec3 cameraPos;
+
+					out vec3 vOrigin;
+					out vec3 vDirection;
+
+					void main() {
+						vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+
+						vOrigin = vec3( inverse( modelMatrix ) * vec4( cameraPos, 1.0 ) ).xyz;
+						vDirection = position - vOrigin;
+
+						gl_Position = projectionMatrix * mvPosition;
+					}
+				`;
+
+				const fragmentShaderFile = await fetch("shaders/cloud.fs.glsl");
+        const fragmentShader = await fragmentShaderFile.text();
+        console.log(fragmentShader);
+
+				const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+				material = new THREE.RawShaderMaterial( {
+					glslVersion: THREE.GLSL3,
+					uniforms: {
+						base: { value: new THREE.Color( 0xda5449 ) },
+						map: { value: texture },
+						cameraPos: { value: new THREE.Vector3() },
+						threshold: { value: 0.25 },
+						opacity: { value: 0.25 },
+						range: { value: 0.1 },
+						steps: { value: 100 },
+						frame: { value: 0 }
+					},
+					vertexShader,
+					fragmentShader,
+					side: THREE.BackSide,
+					transparent: true
+				} );
+
+				mesh = new THREE.Mesh( geometry, material );
+				scene.add( mesh );
+
+        const parameters = {
+          threshold: 0.25,
+          opacity: 0.25,
+          range: 0.1,
+          steps: 100
+        };
+
+        material.uniforms.threshold.value = parameters.threshold;
+        material.uniforms.opacity.value = parameters.opacity;
+        material.uniforms.range.value = parameters.range;
+        material.uniforms.steps.value = parameters.steps;
     }
     
     setupScene();
@@ -145,6 +239,14 @@ const ThreeDCanvas = () => {
       if(mountains){
         mountains.position.y = -6;
       }
+
+      if(material){
+        mesh.material.uniforms.cameraPos.value.copy( camera.position );
+				mesh.rotation.y = - performance.now() / 7500;
+
+				mesh.material.uniforms.frame.value ++;
+      }
+
       renderer.render(scene, camera);
     };
 
