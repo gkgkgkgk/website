6b71f774c9d336416387fb9c7bb9562224e0d9f6
Gavri Kepets <gkepets@gmail.com>
2023-10-27 12:02:24 -0400

    added better trees?

--- src/app/components/BottomCanvas/index.jsx
+++ src/app/components/BottomCanvas/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import "./styles.css";
 import {fromBlob} from 'geotiff'
-import { darken, lighten } from 'color2k';
+import { desaturate, lighten } from 'color2k';
 const { Noise } = require('noisejs');
 
 const BottomCanvas = (props) => {
@@ -37,10 +37,43 @@ const BottomCanvas = (props) => {
       }
     }
 
+    const drawTree = (size, x, y) => {
+      var trunkWidth = size;
+      var trunkHeight = size * 10;
+      var branchWidth = size * 5;
+
+      // Set the coordinates of the bottom of the trunk
+      var trunkX = x - trunkWidth / 2;
+      var trunkY = y - trunkHeight;
+
+      // Set the color for the silhouette
+      ctx.fillStyle = "#140e0d";
+
+      // Draw the trunk
+      ctx.fillRect(trunkX - trunkWidth / 2, trunkY, trunkWidth, trunkHeight);
+      ctx.fillStyle = "#151a15";
+
+      // Draw the tree branches
+      ctx.beginPath();
+      ctx.moveTo(trunkX, trunkY - branchWidth * 3);
+      ctx.lineTo(trunkX - branchWidth, trunkY);
+      ctx.lineTo(trunkX + branchWidth, trunkY);
+      ctx.fill();
+
+      ctx.beginPath();
+      ctx.moveTo(trunkX, trunkY + 10 - branchWidth * 3);
+      ctx.lineTo(trunkX - branchWidth, trunkY + 10- branchWidth);
+      ctx.lineTo(trunkX + branchWidth, trunkY + 10- branchWidth);
+      ctx.fill();
+    }
+
     const generateCumulusCloud = () => {
       let scale = 10;
-      for (let x = 0; x < 100; x++) {
-        for (let y = 0; y < 100; y++) {
+      let size = 200;
+      let posx = 200;
+      let posy = 300;
+      for (let x = 0; x < size; x++) {
+        for (let y = 0; y < size; y++) {
           const l1 = noise.perlin2(x / scale, y / scale) * 0.5;
           const l2 = noise.perlin2(x / 32, y / 32) * 0.5;
           const l3 = noise.perlin2(x, y) * 0.5;
@@ -49,7 +82,7 @@ const BottomCanvas = (props) => {
           const alpha = ((value + 1) / 2);
 
           ctx.fillStyle = `rgba(${alpha * 255}, ${alpha * 255}, ${alpha * 255}, ${alpha})`;
-          ctx.fillRect(x, y, 1, 1);
+          ctx.fillRect(x + posx, y + posy, 1, 1);
         }
       }
     }
@@ -82,8 +115,7 @@ const BottomCanvas = (props) => {
 
       const minValue = Math.min(...rawElevationData);
       const maxValue = Math.max(...rawElevationData);
-      console.log(minValue, maxValue)
-      const elevationData = rawElevationData.map((value) => (value - minValue) / (maxValue - minValue));  
+      const elevationData = rawElevationData.map((value) => Math.pow((value - minValue) / (maxValue - minValue), 2));  
 
       let line = getRowOrColumn(elevationData, 60, 50, slice, true);
       mountain.heights = line;
@@ -100,23 +132,35 @@ const BottomCanvas = (props) => {
       canvas.height = container.clientHeight * devicePixelRatio;
     };
 
+    function lerp(start, end, t) {
+      return start * (1 - t) + end * t;
+    }
+
     const drawMountains = () => {
-      let darkenFactor = 0.15/mountains.length;
+      let darkenFactor = 0.2/mountains.length;
+      let desatFactor = 0.2/mountains.length;
       let colors = mountains.map((m, i) => { 
-        return lighten('#1f0d00', i * darkenFactor)
+        return desaturate(lighten('#30130f', i * darkenFactor), i * desatFactor)
       }).reverse()
 
-      let heightOffset = 50;
+      let heightOffset = 40;
       let mountainCount = mountains.length
 
       mountains.forEach((m, im) => {
-        let scaleFactor = 0.5 * (canvas.height - (heightOffset * (mountainCount - im)));
+        let scaleFactor = 0.35 * (canvas.height - (heightOffset * (mountainCount + 1 - im)));
+        let top = canvas.height;
 
         ctx.beginPath();
         ctx.moveTo(0, canvas.height);
         m.heights.forEach((h, i) => {
-          let nh = (canvas.height - (h * scaleFactor)) - (heightOffset * (mountainCount - im))
-          ctx.lineTo(canvas.width * i / 50, nh);
+          let nh = (canvas.height - (h * scaleFactor)) - (heightOffset * (mountainCount + 1 - im))
+          
+          if(nh < top){
+            top = nh;
+          }
+          let x = canvas.width * i / 50;
+
+          ctx.lineTo(x, nh);
           if(i == m.heights.length - 1){
             ctx.lineTo(canvas.width, nh);
           }
@@ -125,19 +169,66 @@ const BottomCanvas = (props) => {
         ctx.lineTo(canvas.width, canvas.height);
         ctx.closePath();
   
-        ctx.fillStyle = colors[im];
-        ctx.fill();
+        if(im == mountainCount-1){
+          ctx.fillStyle = colors[im];
+          ctx.fill();
+        } else {
+          const gradient = ctx.createLinearGradient(0, top, 0, canvas.height);
+          gradient.addColorStop(0.25, colors[im]);
+          gradient.addColorStop(1.0, lighten(colors[im], 0.075));
+          ctx.fillStyle = gradient;
+          ctx.fill();
+        }
+
+        m.heights.forEach((h, i) => {
+          if(i !== 0){
+            let x1 = canvas.width * (i - 1) / 50;
+            let y1 = (canvas.height - (m.heights[i-1] * scaleFactor)) - (heightOffset * (mountainCount + 1 - im))
+
+            let x2 = canvas.width * i / 50;
+            let y2 = (canvas.height - (h * scaleFactor)) - (heightOffset * (mountainCount + 1 - im))
+
+            if(h < 0.5){
+              for (let i = 0; i <= 20 * (mountainCount - im + 1); i++) {
+                const t = i / (20);
+                const x = lerp(x1, x2, t);
+                const y = lerp(y1, y2, t);
+                drawTree(0.075 * im * im, x, y + Math.random() * 100)
+              }
+            }
+        }
+        })
+
       })
     }
 
     const fillSky = () => {
       const skyGradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
-      skyGradient.addColorStop(0, "#37379A");
-      skyGradient.addColorStop(0.5, "#37379a");
-      skyGradient.addColorStop(0.65, "#bd413f");
-      skyGradient.addColorStop(1, "#f39f3f"); 
+      skyGradient.addColorStop(0.1, "#112644");
+      skyGradient.addColorStop(0.25, "#ff7c2a");
+      skyGradient.addColorStop(0.45, "#ff8000");
+      skyGradient.addColorStop(0.65, "#ffb300");
+      skyGradient.addColorStop(1, "#f39b41"); 
       ctx.fillStyle = skyGradient;
       ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+
+      var gradient = ctx.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width/5);
+      gradient.addColorStop(0, "rgb(254, 238, 202, 1)");
+      gradient.addColorStop(1, "rgb(254, 238, 202, 0)");
+
+      ctx.fillStyle = gradient;
+      ctx.beginPath();
+      ctx.arc(canvas.width / 2, canvas.height / 2, canvas.width/5, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.closePath();
+
+      ctx.fillStyle = "#feeeca";
+      ctx.beginPath();
+      ctx.arc(canvas.width / 2, canvas.height / 2, canvas.width/10, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.closePath();
     }
 
 
@@ -145,8 +236,6 @@ const BottomCanvas = (props) => {
       const deltaTime = (timestamp - lastTimestamp) / 1000;
       lastTimestamp = timestamp;
       // fillSky();
-      drawMountains();
-
       requestId = requestAnimationFrame(draw);
     };
 
@@ -158,20 +247,21 @@ const BottomCanvas = (props) => {
 
     const promises = [];
 
-    // promises.push(generateRandomMountain(90));
-    // promises.push(generateRandomMountain(75));
+    promises.push(generateRandomMountain(45));
+    promises.push(generateRandomMountain(35));
     promises.push(generateRandomMountain(25));
-    promises.push(generateRandomMountain(49));
     promises.push(generateRandomMountain(15));
-    promises.push(generateRandomMountain(0));
+    promises.push(generateRandomMountain(5));
 
     Promise.all(promises).then(() => {
       mountains.sort((a, b) => b.slice - a.slice);
 
-      draw();
+      drawMountains();
     });
 
-    generateCumulusCloud();
+    
+
+    // generateCumulusCloud();
 
     return () => {
       window.removeEventListener("resize", resizeCanvas);
