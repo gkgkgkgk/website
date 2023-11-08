d7854d6487a07b610fa24d3f1ae9a181d18425a3
Gavri Kepets <gkepets@gmail.com>
2023-11-08 18:02:26 -0500

    sync

--- public/shaders/fragment.glsl
+++ public/shaders/fragment.glsl
@@ -32,9 +32,9 @@ float noise (in vec2 st) {
 
 vec4 backgroundGradient(float x, float y) {
     vec4 top = vec4(0.027, 0.494, 0.627, 1.0);
-    vec4 bottom = vec4(0.992, 0.722, 0.365, 1.0);
+    vec4 bottom = vec4(0.992, 0.55, 0.165, 1.0);
 
-    vec4 col = mix(bottom, top, y/1.0);
+    vec4 col = mix(bottom, top, pow(y/1.0, 5.0));
 
     return col;
 }
@@ -43,9 +43,16 @@ vec4 sun(vec2 pos, inout vec4 col) {
     vec2 center = vec2(0.5* _width, 0.5 * _height);
     float dist = distance(pos, center);
 
-    if (dist < 100.0) {
-        col = vec4(1.0, 0.8, 0.65, 1.0);
+    if(_width > _height){
+        if (dist < _width / 25.0) {
+                col = vec4(1.0, 0.8, 0.65, 1.0);
+        }
+    } else {
+        if (dist < _height / 25.0) {
+                col = vec4(1.0, 0.8, 0.65, 1.0);
+        }
     }
+    
 
     // if (dist > 0.1 && dist < 1.0) {
     //     col = mix(vec4(1.0, 0.45, 0.15, 1.0), col, pow((dist), 1.0));
@@ -55,40 +62,73 @@ vec4 sun(vec2 pos, inout vec4 col) {
 }
 
 vec4 mountains(vec2 pos, inout vec4 col) {
-    float n = noise(vec2(pos.x/_width, 0.0)) / 2.0;
-    n += noise(vec2(pos.x/_width * 8.0, 0.0)) / 4.0;
-    n += noise(vec2(pos.x/_width * 16.0, 0.0)) / 8.0;
-    n += noise(vec2(pos.x/_width * 128.0, 0.0)) / 16.0;
+    float d = 0.0;
+    float nd = 0.0;
 
-    if (pos.y < n * (_height/2.0)){
-        col = vec4(0.25, 0.25, 0.25, 1.0);
-
-        vec2 center = vec2(0.5* _height, 0.5) ;
-        vec2 adjustedPos = vec2(pos.x * _height, pos.y);
-        float dist = distance(adjustedPos, center);
-
-        if(dist < 1.0){
-            col = mix(vec4(1.0, 0.45, 0.15, 1.0), col, pow((dist), 1.0));
-        }
+    if(_width > _height){
+        d = _width;
+        nd = _height;
+    } else {
+        d = _height;
+        nd = _width;
     }
 
-    float n1 = noise(vec2(pos.x/_width, 1.0)) / 2.0;
-    n1 += noise(vec2(pos.x/_width * 8.0, 1.0)) / 4.0;
-    n1 += noise(vec2(pos.x/_width * 16.0, 1.0)) / 8.0;
-    n1 += noise(vec2(pos.x/_width * 128.0, 1.0)) / 16.0;
+    float n = noise(vec2(pos.x/d, 0.0)) / 2.0;
+    n += noise(vec2(pos.x/d * 8.0, 0.0)) / 4.0;
+    n += noise(vec2(pos.x/d * 16.0, 0.0)) / 8.0;
+    n += noise(vec2(pos.x/d * 128.0, 0.0)) / 64.0;
 
-    if (pos.y < n1 * (_height/2.0) - 100.0){
+    if (pos.y < (_height/2.0) * n + (_height/3.0)){
+        col = mix(vec4(0.25, 0.25, 0.25, 1.0), vec4(1.0, 0.45, 0.15, 1.0), 0.25);
+
+        vec2 center = vec2(0.5* _width, 0.5 * _height) ;
+        float dist = distance(pos, center);
+
+        // if(dist < 250.0){
+        //     col = mix(vec4(1.0, 0.45, 0.15, 1.0), col, pow((dist)/ 250.0, 1.0));
+        // }
+    }
+
+    float n1 = noise(vec2(pos.x/d, 1.0)) / 2.0;
+    n1 += noise(vec2(pos.x/d * 8.0, 1.0)) / 4.0;
+    n1 += noise(vec2(pos.x/d * 16.0, 1.0)) / 8.0;
+    n1 += noise(vec2(pos.x/d * 128.0, 1.0)) / 64.0;
+
+    if (pos.y < (_height/2.0) * n1 + (_height/6.0)){
         col = vec4(0.15, 0.15, 0.15, 1.0);
 
-        vec2 center = vec2(0.5* _height, 0.5) ;
-        vec2 adjustedPos = vec2(pos.x * _height, pos.y);
-        float dist = distance(adjustedPos, center);
+        vec2 center = vec2(0.5* _width, 0.5 * _height) ;
+        float dist = distance(pos, center);
 
-        if(dist < 1.0){
-            col = mix(vec4(1.0, 0.45, 0.15, 1.0), col, pow((dist), 1.0));
-        }
+        // if(dist < 500.0){
+        //     col = mix(vec4(1.0, 0.45, 0.15, 1.0), col, pow((dist)/ 500.0, 0.5));
+        // }
     }
 
+    float n2 = noise(vec2(pos.x/d, 2.0)) / 2.0;
+    n2 += noise(vec2(pos.x/d * 8.0, 2.0)) / 4.0;
+    n2 += noise(vec2(pos.x/d * 16.0, 2.0)) / 8.0;
+    n2 += noise(vec2(pos.x/d * 128.0, 2.0)) / 64.0;
+
+    if (pos.y < (_height/4.0) * n2 + (_height/12.0)){
+        col = vec4(0.1, 0.1, 0.1, 1.0);
+
+        vec2 center = vec2(0.5* _width, 0.5 * _height) ;
+        float dist = distance(pos, center);
+
+        // if(dist < 1000.0){
+        //     col = mix(vec4(1.0, 0.45, 0.15, 1.0), col, pow((dist)/ 1000.0, 0.25));
+        // }
+    }
+
+
+    return col;
+}
+
+vec4 trees(vec2 pos, inout vec4 col){
+    // if(pos.y % 2.0 == 0){
+    //     col = vec4(1.0, 1.0, 1.0, 1.0);
+    // }
     return col;
 }
 
@@ -98,6 +138,7 @@ void main() {
     col = backgroundGradient(vUv.x, vUv.y);
     col = sun(pos, col);
     col = mountains(pos, col);
+    col = trees(pos, col);
 
     gl_FragColor = (col);
 }
